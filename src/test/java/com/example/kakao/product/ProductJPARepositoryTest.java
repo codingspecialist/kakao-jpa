47c91ba5ed307e75d93a68bca4ff36ba70e6e851
metacoding <getinthere@naver.com>
2023-07-09 18:51:12 +0900

    TestEntityManager로 변경

--- src/test/java/com/example/kakao/product/ProductJPARepositoryTest.java
+++ src/test/java/com/example/kakao/product/ProductJPARepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -24,7 +25,7 @@ import java.util.stream.Collectors;
 public class ProductJPARepositoryTest extends DummyEntity {
 
     @Autowired
-    private EntityManager em;
+    private TestEntityManager em;
 
     @Autowired
     private ProductJPARepository productJPARepository;
